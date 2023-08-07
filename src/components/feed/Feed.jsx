55ad35f1b09766f56279a05659353f87134a2efc
likhith <kumarlikhith675@gmail.com>
2023-08-07 15:39:13 +0530

    post is added to feed

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,7 +1,17 @@
+import Share from '../share/share';
+import Post from '../post/Post';
+import './feed.css';
+
 export default function Feed() {
     return (
         <>
-
+            <div className="feed">
+                <div className="feedWrapper">
+                    <Share />
+                    <Post />
+                    
+                </div>
+            </div>
         </>
     );
 }
