a4f486b1823db0381efa7e413fd7382a566afb05
likhith <kumarlikhith675@gmail.com>
2023-08-07 15:36:49 +0530

    sidebar over

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,147 @@
+import './sidebar.css';
+import { RssFeed,Group,PlayCircleFilledOutlined,Bookmark,HelpOutline,WorkOutline } from '@mui/icons-material';
 export default function Sidebar() {
     return (
         <>
-            side bar
+            <div className="sidebar">
+                <div className="sidebarWrapper">
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <RssFeed className="sidebarIcon" />
+                            <span className="sidebarListItemText">Feed</span>
+                        </li>
+                        <li className="sidebarListItem">
+                            <PlayCircleFilledOutlined className="sidebarIcon" />
+                            <span className="sidebarListItemText">Groups</span>
+                        </li>
+                        <li className="sidebarListItem">
+                            <Group className="sidebarIcon" />
+                            <span className="sidebarListItemText">Groups</span>
+                        </li>
+                        <li className="sidebarListItem">
+                            <Bookmark className="sidebarIcon" />
+                            <span className="sidebarListItemText">Bookmarks</span>
+                        </li>
+                        <li className="sidebarListItem">
+                            <HelpOutline className="sidebarIcon" />
+                            <span className="sidebarListItemText">Questions</span>
+                        </li>
+                        <li className="sidebarListItem">
+                            <WorkOutline className="sidebarIcon" />
+                            <span className="sidebarListItemText">Jobs</span>
+                        </li>
+                    </ul>
+                    <button className="sidebarButton">show more</button>
+                    <hr className="sidebarHr" />
+                    <ul className="sidebarFriendList">
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        <li className="sidebarFriend">
+                            <img src="/assets/person/person-1.png" alt="" className="sidebarFriendImg" />
+                            <span className="sidebarImageText">
+                                Cicily
+                            </span>
+                        </li>
+                        
+                    </ul>
+                  
+                </div>
+            </div>
         </>
     );
 }
