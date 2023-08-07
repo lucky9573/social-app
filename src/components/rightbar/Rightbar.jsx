c50698e35e38e89c628defd46fbb427bc8611e7e
likhith <kumarlikhith675@gmail.com>
2023-08-07 15:37:22 +0530

    right and share bar over

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,11 @@
+import './rightbar.css';
+
 export default function Rightbar() {
     return (
         <>
-            
+            <div className="rightbar">
+                <h5>Right bar</h5>
+            </div>
         </>
     );
 }
