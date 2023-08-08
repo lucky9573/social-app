a915b2ffc7f17ca6b5b09c331fbfa5e158c41a31
likhith <kumarlikhith675@gmail.com>
2023-08-08 22:56:09 +0530

    post updated

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,19 +1,17 @@
 import Share from '../share/share';
 import Post from '../post/Post';
 import './feed.css';
-import { Posts } from '../../dummyData';
+import { Posts, Users } from '../../dummyData';
 export default function Feed() {
     return (
         <>
             <div className="feed">
                 <div className="feedWrapper">
                     <Share />
-               { Posts.map((p) => {
-                    <Post key={p.id} post={p} />
-               })
-                }
-
-                    
+               { Posts.map((p,u) => (
+                    <Post key={p.id} post={p} users={u} />
+               ))
+                }                    
                     
                 </div>
             </div>
